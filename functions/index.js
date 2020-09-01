c0fea64d6ddea1116759e119e67c20b9fc219aac
KyeongHoSeong <dasomaru@gmail.com>
2020-09-01 09:06:19 +0900

    add express and use api

--- functions/index.js
+++ functions/index.js
@@ -3,25 +3,46 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// post data to firestore
-exports.createScream = functions.https.onRequest((req,res) => {
-    if(req.method !== 'POST') {
-        return res.status(400).json({ error: 'Method not allowed' })
-    }
+// use express~~
+const express = require('express');
+const app = express();
 
-    //define data structure
+
+app.get('/screams', (req, res) => {
+    admin
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then(data => {
+            let screams = [];
+            data.forEach(doc=> {
+                //screams.push(doc.data());
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
+            });
+            return res.json(screams);
+        })
+        .catch(err => console.error(err));
+});
+
+app.post('/scream', (req,res) => {  
     const newScreams = {
         body:req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        //createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     };
 
-    // post
     admin.firestore()
         .collection('screams')
         .add(newScreams)
         .then(doc => {
-            res.json({message: `document ${doc.id} created successfully`});
+            res.json({messabe: `document ${doc.id} created successfully`});
         })
         .catch(err=> {
             res.status(500).json({eror: 'something went wrong' });
@@ -29,18 +50,33 @@ exports.createScream = functions.https.onRequest((req,res) => {
         });
 });
 
-// get data from firestore by use of firebase-admin
-exports.getScreams = functions.https.onRequest((req,res) => {
-    admin
-      .firestore()
-      .collection("screams")
-      .get()
-      .then((data) => {
-        let screams = [];
-        data.forEach((doc) => {
-          screams.push(doc.data());
-        });
-        return res.json(screams);
-      })
-      .catch((err) => console.error(err));
-});
+ 
+// exports.getScreams = functions.https.onRequest((req,res) => {
+    
+// })
+
+// exports.createScream = functions.https.onRequest((req,res) => {
+//     if(req.method !== 'POST') {
+//         return res.status(400).json({ error: 'Method not allowed' })
+//     }
+//     const newScreams = {
+//         body:req.body.body,
+//         userHandle: req.body.userHandle,
+//         createdAt: admin.firestore.Timestamp.fromDate(new Date())
+//     };
+
+//     admin.firestore()
+//         .collection('screams')
+//         .add(newScreams)
+//         .then(doc => {
+//             res.json({messabe: `document ${doc.id} created successfully`});
+//         })
+//         .catch(err=> {
+//             res.status(500).json({eror: 'something went wrong' });
+//             console.error(err);
+//         });
+// });
+
+// https://baseurl.com/api/
+exports.api = functions.https.onRequest(app); 
+
