279fad0f14784af7fe79ee8ac8de8521daffff10
KyeongHoSeong <54465859+KyeongHoSeong@users.noreply.github.com>
2020-09-09 02:10:20 +0900

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,14 +5,7 @@ const app = require('express')();
 admin.initializeApp();
 //step 6
 const firebaseConfig = {
-    apiKey: "AIzaSyAf3mgT3HiJAhGLuT9HN80A3E0fp-nTe9M",
-    authDomain: "fireactplayground.firebaseapp.com",
-    databaseURL: "https://fireactplayground.firebaseio.com",
-    projectId: "fireactplayground",
-    storageBucket: "fireactplayground.appspot.com",
-    messagingSenderId: "860050537620",
-    appId: "1:860050537620:web:d1d568049934984a06c881",
-    measurementId: "G-LZH00LGQEY"
+   
   };
   
 const firebase = require('firebase');
